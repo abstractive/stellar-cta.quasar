8cac831b5db2acbd3bbf9cee55694c4786af910f
digitalextremist // <code@extremist.digital>
2019-12-20 03:30:03 -0800

    directly change i18n messages

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -5,7 +5,6 @@ import messages from '../i18n'
 Vue.use(VueI18n)
 
 export default ({ app }) => {
-  console.info(["forms: i18n before", app])
   if (typeof app.i18n === 'undefined') {
     app.i18n = new VueI18n({
       locale: 'en-us',
@@ -13,10 +12,13 @@ export default ({ app }) => {
       messages
     })
   } else {
+    _.assign(app.i18n.messages['en-us'], app.i18n.messages['en-us'], messages['en-us'])
+    /*
     app.i18n.mergeLocaleMessage({
       locale: 'en-us',
       messages
     })
+    */
   }
   console.info(["forms: i18n after", app.i18n])
 }
