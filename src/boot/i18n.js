9ad8646a681c530aa0fb8bd91b02a56d80d13544
digitalextremist // <code@extremist.digital>
2019-12-20 02:58:31 -0800

    tracking down how to pass i18n from extensions

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -3,7 +3,7 @@ import VueI18n from 'vue-i18n'
 import messages from '../i18n'
 
 Vue.use(VueI18n)
-console.log(['forms', messages])
+
 export default ({ app }) => {
   if (typeof app.i18n === 'undefined') {
     console.warn("forms: no i18n")
@@ -16,7 +16,7 @@ export default ({ app }) => {
     console.info("forms: has i18n")
     app.i18n.mergeLocaleMessage({
       locale: 'en-us',
-      messages
+      messages: messages['en-us']
     })
   }
   console.info(["forms: i18n", app.i18n])
