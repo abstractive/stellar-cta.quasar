1407f14839748982257b91ef58c481da17676713
digitalextremist // <code@extremist.digital>
2019-12-20 02:20:01 -0800

    leaking debug output for a moment

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import messages from 'src/i18n'
+import messages from '../i18n'
 
 Vue.use(VueI18n)
-
+console.log(messages)
 export default ({ app }) => {
   if (typeof app.i18n === 'undefined') {
+    console.warn("no i18n")
     app.i18n = new VueI18n({
       locale: 'en-us',
       fallbackLocale: 'en-us',
       messages
     })
   } else {
+    console.info("has i18n")
     app.i18n.mergeLocaleMessage({
       locale: 'en-us',
       messages
