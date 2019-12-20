66019d92e45b4fc8f863f6f7424f3fd37ac5aaf7
digitalextremist // <code@extremist.digital>
2019-12-20 03:16:40 -0800

    try to override i18n from extension

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -5,12 +5,13 @@ import messages from '../i18n'
 Vue.use(VueI18n)
 
 export default ({ app }) => {
-  if (typeof app.i18n === 'undefined') {
-    app.i18n = new VueI18n({
-      locale: 'en-us',
-      fallbackLocale: 'en-us',
-      messages
-    })
+  // if (typeof app.i18n === 'undefined') {
+  app.i18n = new VueI18n({
+    locale: 'en-us',
+    fallbackLocale: 'en-us',
+    messages
+  })
+  /*
   } else {
     console.info(["forms: has i18n", messages])
     app.i18n.mergeLocaleMessage({
@@ -19,4 +20,6 @@ export default ({ app }) => {
     })
     console.info(["forms: i18n after", app.i18n])
   }
+  */
+  console.info(["forms: i18n after", app.i18n])
 }
