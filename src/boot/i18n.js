85523ddf369ee876235a5cbeaa9774c5abddc575
digitalextremist // <code@extremist.digital>
2019-12-20 02:49:38 -0800

    improve debug output

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -3,20 +3,21 @@ import VueI18n from 'vue-i18n'
 import messages from '../i18n'
 
 Vue.use(VueI18n)
-console.log(messages)
+console.log(['forms', messages])
 export default ({ app }) => {
   if (typeof app.i18n === 'undefined') {
-    console.warn("no i18n")
+    console.warn("forms: no i18n")
     app.i18n = new VueI18n({
       locale: 'en-us',
       fallbackLocale: 'en-us',
       messages
     })
   } else {
-    console.info("has i18n")
+    console.info("forms: has i18n")
     app.i18n.mergeLocaleMessage({
       locale: 'en-us',
       messages
     })
   }
+  console.info(["forms: i18n", app.i18n])
 }
