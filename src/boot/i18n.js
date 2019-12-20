ad65235b0ff9684a44e850e73d0d584c707a3e60
digitalextremist // <code@extremist.digital>
2019-12-20 03:22:47 -0800

    fix method param and try again with i18n

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -5,21 +5,17 @@ import messages from '../i18n'
 Vue.use(VueI18n)
 
 export default ({ app }) => {
-  // if (typeof app.i18n === 'undefined') {
-  app.i18n = new VueI18n({
-    locale: 'en-us',
-    fallbackLocale: 'en-us',
-    messages
-  })
-  /*
+  if (typeof app.i18n === 'undefined') {
+    app.i18n = new VueI18n({
+      locale: 'en-us',
+      fallbackLocale: 'en-us',
+      messages
+    })
   } else {
-    console.info(["forms: has i18n", messages])
     app.i18n.mergeLocaleMessage({
       locale: 'en-us',
-      messages: messages['en-us']
+      message: messages['en-us']
     })
-    console.info(["forms: i18n after", app.i18n])
   }
-  */
   console.info(["forms: i18n after", app.i18n])
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function extendConf(conf) {
   conf.framework.plugins.push('Notify')
   conf.framework.plugins.push('Dialog')
   conf.boot.push('~quasar-app-extension-stellar-forms/src/boot/i18n.js')
+  conf.boot.push('~quasar-app-extension-stellar-forms/src/boot/axios.js')
   conf.boot.push('~quasar-app-extension-stellar-forms/src/boot/lodash.js')
   conf.boot.push('~quasar-app-extension-stellar-forms/src/boot/vuelidate.js')
   conf.boot.push('~quasar-app-extension-stellar-forms/src/boot/vuelidate-mixins.js')
